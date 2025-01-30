0ef1307aa8be83885717821858f69641daf3b556
Adriano Lima <dri.limasantos@gmail.com>
2025-01-30 15:48:54 -0300

    UP

--- passaros/database/regras.py
+++ passaros/database/regras.py
@@ -1,27 +1,28 @@
 # VAGAS TRAVADAS
 
-# # vAGAS BOAS
+# VAGAS BOAS
 # Bloco: 28; Apartamento: 43; Vaga: 34; (545, 681) "OK"
 # Bloco: 14; Apartamento: 44; Vaga: 190; (277, 837) "OK"
 # Bloco: 29; Apartamento: 12; Vaga: 43; (552, 690) "OK"
 
-# # vAGAS RUINS
+# VAGAS RUINS
 # Bloco: 11; Apartamento: 31; Vaga: 320; (211, 967) "OK"
 # Bloco: 24; Apartamento: 4; Vaga: 256; (459, 903)
 # Bloco: 28; Apartamento: 42; Vaga: 17; (544, 665) "OK"
 # Bloco: 3; Apartamento: 44; Vaga: 409; (60, 1056) "OK"
 
+# VAGAS PNE
 
 
-
-# vAGAS BOAS
+# VAGAS BOAS
 (545, 681), 
 (277, 837), 
 (552, 690), 
+# VAGAS RUINS
 (211, 967), 
 (459, 903), 
 (544, 665), 
 (60, 1056), 
-
+# VAGAS PNE
 
 
