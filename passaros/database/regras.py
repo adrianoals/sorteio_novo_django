6d56a27342ec4498457bdbae7f2f970492986448
dri.limasantos@gmail.com <dri.limasantos@gmail.com>
2025-01-30 16:06:37 -0300

    up

--- passaros/database/regras.py
+++ passaros/database/regras.py
@@ -1,17 +1,36 @@
 # VAGAS TRAVADAS
 
 # VAGAS BOAS
-# Bloco: 28; Apartamento: 43; Vaga: 34; (545, 681) "OK"
 # Bloco: 14; Apartamento: 44; Vaga: 190; (277, 837) "OK"
+# Bloco: 28; Apartamento: 43; Vaga: 34; (545, 681) "OK"
 # Bloco: 29; Apartamento: 12; Vaga: 43; (552, 690) "OK"
 
 # VAGAS RUINS
+# Bloco: 3; Apartamento: 44; Vaga: 409; (60, 1056) "OK"
 # Bloco: 11; Apartamento: 31; Vaga: 320; (211, 967) "OK"
 # Bloco: 24; Apartamento: 4; Vaga: 256; (459, 903)
 # Bloco: 28; Apartamento: 42; Vaga: 17; (544, 665) "OK"
-# Bloco: 3; Apartamento: 44; Vaga: 409; (60, 1056) "OK"
 
 # VAGAS PNE
+"""
+
+Bloco: 09; Apartamento: 24; Vaga: 648; 
+Bloco: 11; Apartamento: 04; Vaga: 221;
+Bloco: 15; Apartamento: 04; Vaga: 187;
+Bloco: 16; Apartamento: 24; Vaga: 181;
+Bloco: 17; Apartamento: 22; Vaga: 174;
+Bloco: 19; Apartamento: 22; Vaga: 161;
+Bloco: 22; Apartamento: 44; Vaga: 141;
+Bloco: 26; Apartamento: 04; Vaga: 117;
+Bloco: 27; Apartamento: 12; Vaga: 29;
+Bloco: 28; Apartamento: 03; Vaga: 37; 
+Bloco: 30; Apartamento: 11; Vaga: 49;
+Bloco: 30; Apartamento: 13; Vaga: 50;
+Bloco: 31; Apartamento: 01; Vaga: 57;
+Bloco: 31; Apartamento: 04; Vaga: 58;
+
+
+"""
 
 
 # VAGAS BOAS
@@ -24,5 +43,6 @@
 (544, 665), 
 (60, 1056), 
 # VAGAS PNE
+
 
 
