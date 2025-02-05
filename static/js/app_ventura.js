9554c8fbb0464bcb14222ef17abcf04a68bbebcb
Adriano Lima <dri.limasantos@gmail.com>
2025-02-05 18:19:50 -0300

    up

--- static/js/app_ventura.js
+++ static/js/app_ventura.js
@@ -1,56 +1,16 @@
 function iniciarSorteio() {
-    // Oculta todo o conteúdo do container
-    var container = document.getElementById('container-sorteio');
-    var titulo = document.querySelector("h5.font_subtitle"); // Seleciona o título "SELECIONANDO OS APARTAMENTOS PRESENTES!"
-
-    if (container) container.style.display = "none"; // Oculta o container
-    if (titulo) titulo.style.display = "block"; // Garante que o título continua visível
-
-    // Exibe apenas a animação do GIF
+    // Oculta todo o conteúdo do sorteio, mas mantém o título e o GIF visíveis
+    document.getElementById('conteudoSorteio').style.display = 'none';
     document.getElementById('loadingAnimation').style.display = 'block';
 
-    // Aguarda 2.2 segundos antes de exibir o conteúdo de volta e sortear o apartamento
+    // Espera 2.2 segundos antes de submeter o formulário
     setTimeout(function() {
         document.querySelector('form').submit();
     }, 2200);
 }
 
-// Adiciona o evento ao botão de sorteio
+// Adiciona evento ao botão de sorteio
 document.querySelector('[type="submit"]').addEventListener('click', function(event) {
-    event.preventDefault(); // Previne o envio imediato do formulário
-    iniciarSorteio();
+    event.preventDefault(); // Previne o envio imediato
+    iniciarSorteio(); // Inicia o efeito visual e o sorteio
 });
-
-
-
-
-// function iniciarSorteio() {
-//     // Oculta o resultado do sorteio anterior, mensagens e o rótulo do síndico selecionado, se existirem
-//     var resultadoAnterior = document.getElementById('sindico');
-//     var mensagens = document.querySelector('.alert');
-//     var labelSindico = document.getElementById('labelSindico'); // Seleciona o rótulo pelo id
-    
-//     if (resultadoAnterior) resultadoAnterior.style.display = 'none';
-//     if (mensagens) mensagens.style.display = 'none';
-//     if (labelSindico) labelSindico.style.display = 'none'; // Oculta o rótulo
-
-//     // Mostra a animação
-//     document.getElementById('loadingAnimation').style.display = 'block';
-    
-//     // Oculta o formulário de sorteio para evitar múltiplas submissões
-//     document.querySelector('form').style.display = 'none';
-
-//     // Espera 5 segundos antes de submeter o formulário
-//     setTimeout(function() {
-//         // Aqui, em vez de recarregar a página, você submete o formulário
-//         // Isso envia a requisição POST para o servidor
-//         document.querySelector('form').submit();
-//     }, 2200); // 5000 milissegundos = 5 segundos
-// }
-
-// // Adicione um ouvinte de evento ao seu botão de sorteio para iniciar o processo
-// document.querySelector('[type="submit"]').addEventListener('click', function(event) {
-//     event.preventDefault(); // Previne o comportamento padrão do formulário
-//     iniciarSorteio();
-// });
-
