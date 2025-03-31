c4a1d0f5577283e72017ae94d65fbef9a4f1c703
Adriano Lima <dri.limasantos@gmail.com>
2025-03-31 11:28:08 -0300

    up

--- static/js/app.js
+++ static/js/app.js
@@ -4,31 +4,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function revelarLinhasProgressivamente() {
     const linhas = document.querySelectorAll('.linha-apartamento');
-    const loadingContainer = document.getElementById('loadingContainer'); // Certifique-se de que este ID corresponde ao seu HTML
+    const loadingContainer = document.getElementById('loadingContainer');
 
     if (linhas.length > 0) {
         linhas.forEach((linha, index) => {
             setTimeout(() => {
-                linha.style.opacity = 1; // Faz a linha aparecer
-                linha.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+                linha.style.opacity = 1; // Faz a linha aparecer gradualmente
 
-                // Se for a última linha, ajusta o scroll para o final e esconde a animação de carregamento
+                // Rola a página até a linha que acabou de se tornar visível
+                linha.scrollIntoView({ behavior: 'smooth', block: 'end' });
+
+                // Verifica se é a última linha
                 if (index === linhas.length - 1) {
+                    // Aguarda a última linha ficar visível antes de esconder a animação
                     setTimeout(() => {
-                        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-                        // Esconde a animação de carregamento e o container
                         loadingContainer.style.display = 'none';
-                    }, 50); // Tempo adicional para a última linha ser revelada antes de esconder a animação
+                        // Opcional: rola para o fim da página após o último elemento ser revelado
+                        document.body.scrollIntoView({behavior: 'smooth', block: 'end'});
+                    }, 5000); // Aguarda um pouco mais para garantir que a animação de carregamento desapareça suavemente
                 }
-            }, index * 50); // Intervalo entre a revelação de cada linha
+            }, index * 5000); // Ajusta para 250ms entre a revelação de cada linha para tornar a animação mais rápida
         });
     } else {
-        // Se não houver linhas, esconde a animação imediatamente
         loadingContainer.style.display = 'none';
     }
 }
-
-
 
 // document.addEventListener('DOMContentLoaded', function() {
 //     revelarLinhasProgressivamente();
@@ -36,33 +36,27 @@ function revelarLinhasProgressivamente() {
 
 // function revelarLinhasProgressivamente() {
 //     const linhas = document.querySelectorAll('.linha-apartamento');
-//     const loadingContainer = document.getElementById('loadingContainer');
+//     const loadingContainer = document.getElementById('loadingContainer'); // Certifique-se de que este ID corresponde ao seu HTML
 
 //     if (linhas.length > 0) {
 //         linhas.forEach((linha, index) => {
 //             setTimeout(() => {
-//                 linha.style.opacity = 1; // Faz a linha aparecer gradualmente
+//                 linha.style.opacity = 1; // Faz a linha aparecer
+//                 linha.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
 
-//                 // Rola a página até a linha que acabou de se tornar visível
-//                 linha.scrollIntoView({ behavior: 'smooth', block: 'end' });
-
-//                 // Verifica se é a última linha
+//                 // Se for a última linha, ajusta o scroll para o final e esconde a animação de carregamento
 //                 if (index === linhas.length - 1) {
-//                     // Aguarda a última linha ficar visível antes de esconder a animação
 //                     setTimeout(() => {
+//                         window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+//                         // Esconde a animação de carregamento e o container
 //                         loadingContainer.style.display = 'none';
-//                         // Opcional: rola para o fim da página após o último elemento ser revelado
-//                         document.body.scrollIntoView({behavior: 'smooth', block: 'end'});
-//                     }, 500); // Aguarda um pouco mais para garantir que a animação de carregamento desapareça suavemente
+//                     }, 5000); // Tempo adicional para a última linha ser revelada antes de esconder a animação
 //                 }
-//             }, index * 250); // Ajusta para 250ms entre a revelação de cada linha para tornar a animação mais rápida
+//             }, index * 5000); // Intervalo entre a revelação de cada linha
 //         });
 //     } else {
+//         // Se não houver linhas, esconde a animação imediatamente
 //         loadingContainer.style.display = 'none';
 //     }
 // }
-
-
-
-
 
