e4582a581797dd4f3af0635a506a3cb04b64738d
Adriano Lima <dri.limasantos@gmail.com>
2025-04-23 14:15:36 -0300

    up

--- alvorada/urls.py
+++ alvorada/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('alvorada-sorteio/', views.alvorada_sorteio, name='alvorada_sorteio'),
-    path('excel/', views.alvorada_excel, name='alvorada_excel'),
-    path('qrcode/', views.alvorada_qrcode, name='alvorada_qrcode'),
-    path('zerar/', views.alvorada_zerar, name='alvorada_zerar'),
+    path('alvorada-excel/', views.alvorada_excel, name='alvorada_excel'),
+    path('alvorada-qrcode/', views.alvorada_qrcode, name='alvorada_qrcode'),
+    path('alvorada-zerar/', views.alvorada_zerar, name='alvorada_zerar'),
 ] 
