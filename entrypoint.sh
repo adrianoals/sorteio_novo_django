04e42a3ff0324d1c2f595a4e9114725171c14b30
dri.limasantos@gmail.com <dri.limasantos@gmail.com>
2025-05-31 15:46:21 -0300

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# Espera o banco (opcional, pode remover se Supabase for confiável)
 echo "Aguardando banco de dados Supabase ficar disponível..."
 
-# Testa conexão com o host do Supabase (opcional)
-nc -z -v -w30 $DB_HOST $DB_PORT
+nc -z -v -w30 "$DB_HOST" "$DB_PORT"
 if [ $? -eq 0 ]; then
   echo "Banco disponível"
 else
@@ -12,13 +10,10 @@ else
   exit 1
 fi
 
-# Roda as migrations
 echo "Executando migrate..."
 python manage.py migrate --noinput
 
-# Coleta arquivos estáticos (opcional)
 echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput
 
-# Executa o comando padrão (gunicorn)
 exec "$@"
