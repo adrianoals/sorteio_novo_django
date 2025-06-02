f46fd5eec3f9ebbcfa0639457a969bbb66aeb882
dri.limasantos@gmail.com <dri.limasantos@gmail.com>
2025-06-01 21:57:29 -0300

    up

--- helborsorteio/urls.py
+++ helborsorteio/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helborsorteio-sorteio/', views.helborsorteio_sorteio, name='helborsorteio_sorteio'),
-    path('helborsorteio-excel/', views.helborsorteio_excel, name='helborsorteio_excel'),
-    path('helborsorteio-qrcode/', views.helborsorteio_qrcode, name='helborsorteio_qrcode'),
-    path('helborsorteio-zerar/', views.helborsorteio_zerar, name='helborsorteio_zerar'),
+    path('helbor-sorteio/', views.helborsorteio_sorteio, name='helborsorteio_sorteio'),
+    path('helbor-excel/', views.helborsorteio_excel, name='helborsorteio_excel'),
+    path('helbor-qrcode/', views.helborsorteio_qrcode, name='helborsorteio_qrcode'),
+    path('helbor-zerar/', views.helborsorteio_zerar, name='helborsorteio_zerar'),
 ] 
