63d7e2fc6d3fcd9406a3afccea55d633136064f7
Adriano Santos <adriano@solidapps.tech>
2025-11-04 19:03:57 -0300

    fix 3 coelhos

--- sky_view/admin.py
+++ sky_view/admin.py
@@ -1,26 +1,26 @@
-# from django.contrib import admin
-# from .models import Apartamento, Vaga, Sorteio
+from django.contrib import admin
+from .models import Apartamento, Vaga, Sorteio
 
 
-# @admin.register(Apartamento)
-# class ApartamentoAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'numero', 'direito_vaga_dupla', 'direito_duas_vagas_livres', 'is_pne')
-#     list_display_links = ('id', 'numero')
-#     list_editable = ('direito_vaga_dupla', 'direito_duas_vagas_livres', 'is_pne')
-#     list_filter = ('direito_vaga_dupla', 'direito_duas_vagas_livres', 'is_pne')
-#     search_fields = ('numero',)
+@admin.register(Apartamento)
+class ApartamentoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'numero', 'direito_vaga_dupla', 'direito_duas_vagas_livres', 'is_pne')
+    list_display_links = ('id', 'numero')
+    list_editable = ('direito_vaga_dupla', 'direito_duas_vagas_livres', 'is_pne')
+    list_filter = ('direito_vaga_dupla', 'direito_duas_vagas_livres', 'is_pne')
+    search_fields = ('numero',)
 
-# @admin.register(Vaga)
-# class VagaAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'numero', 'subsolo', 'tipo_vaga', 'is_pne')
-#     list_display_links = ('id', 'numero')
-#     list_editable = ('subsolo', 'tipo_vaga', 'is_pne')
-#     list_filter = ('subsolo', 'tipo_vaga', 'is_pne')
-#     search_fields = ('numero',)
+@admin.register(Vaga)
+class VagaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'numero', 'subsolo', 'tipo_vaga', 'is_pne')
+    list_display_links = ('id', 'numero')
+    list_editable = ('subsolo', 'tipo_vaga', 'is_pne')
+    list_filter = ('subsolo', 'tipo_vaga', 'is_pne')
+    search_fields = ('numero',)
 
-# @admin.register(Sorteio)
-# class SorteioAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'apartamento', 'vaga', 'data_sorteio')
-#     list_filter = ('data_sorteio',)
-#     search_fields = ('apartamento__numero', 'vaga__numero')
+@admin.register(Sorteio)
+class SorteioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'apartamento', 'vaga', 'data_sorteio')
+    list_filter = ('data_sorteio',)
+    search_fields = ('apartamento__numero', 'vaga__numero')
 
--- tres_coelhos/urls.py
+++ tres_coelhos/urls.py
@@ -3,31 +3,31 @@ from tres_coelhos.views import tres_coelhos_menu, tres_coelhos_sorteio, tres_coe
 
 urlpatterns = [
         # Rota para o menu principal
-        path('tres-coelhos-menu/', tres_coelhos_menu, name='tres_coelhos_menu'),
+        path('3coelhos-menu/', tres_coelhos_menu, name='tres_coelhos_menu'),
         # Rota para iniciar o sorteio
-        path('tres-coelhos-sorteio/', tres_coelhos_sorteio, name='tres_coelhos_sorteio'), 
+        path('3coelhos-sorteio/', tres_coelhos_sorteio, name='tres_coelhos_sorteio'), 
         # Rota para exportar os resultados do sorteio para um arquivo Excel
         path('sorteio/excel/', tres_coelhos_excel, name='tres_coelhos_excel'),
 
         # Rota para gerar o QR Code do sorteio
-        path('tres-coelhos-qrcode', tres_coelhos_qrcode, name='tres_coelhos_qrcode'),
+        path('3coelhos-qrcode/', tres_coelhos_qrcode, name='tres_coelhos_qrcode'),
         
         # Rota para iniciar o sorteio dupla
-        path('tres-coelhos-dupla/', tres_coelhos_dupla, name='tres_coelhos_dupla'), 
+        path('3coelhos-dupla/', tres_coelhos_dupla, name='tres_coelhos_dupla'), 
         # Rota para exportar os resultados do sorteio para um arquivo Excel
         path('sorteio/dupla/excel/', tres_coelhos_dupla_excel, name='tres_coelhos_dupla_excel'),
         
         # Rota para zerar o sorteio
-        path('tres-coelhos-zerar/', tres_coelhos_zerar, name='tres_coelhos_zerar'),
+        path('3coelhos-zerar/', tres_coelhos_zerar, name='tres_coelhos_zerar'),
 
         # Rota para o resultado
-        path('tres-coelhos-resultado/', tres_coelhos_resultado, name='tres_coelhos_resultado'),
+        path('3coelhos-resultado/', tres_coelhos_resultado, name='tres_coelhos_resultado'),
         # Rota para exportar o resultado completo para Excel
         path('resultado/excel/', tres_coelhos_resultado_excel, name='tres_coelhos_resultado_excel'),
         
         # Rotas para configuração
-        path('tres-coelhos-configurar-pne/', tres_coelhos_configurar_pne, name='tres_coelhos_configurar_pne'),
-        path('tres-coelhos-configurar-duplas/', tres_coelhos_configurar_duplas, name='tres_coelhos_configurar_duplas'),
+        path('3coelhos-configurar-pne/', tres_coelhos_configurar_pne, name='tres_coelhos_configurar_pne'),
+        path('3coelhos-configurar-duplas/', tres_coelhos_configurar_duplas, name='tres_coelhos_configurar_duplas'),
 
 ]
 
